2e2a20b8b77489a25009b221c59362e7c4e14e2f
Erik Espana <espanae@users.noreply.github.com>
2015-09-30 16:25:31 -0400

    Update list-users.php

--- users/list-users.php
+++ users/list-users.php
@@ -1,5 +1,5 @@
 <?php
-// Trying to list all our Cascade users
+// The quest build a "Users, Groups and Roles" diagram has begun by trying to list all Cascade users.
 
 /* This script is based on the following documentation:
 
@@ -14,17 +14,31 @@ require_once( '../auth_espanae_dev.php' );
 try {
     // Get all Cascade users
     $users = $cascade->getUsersByName( "*" );
-    // Troubleshooting statement
     echo "<pre>count(\$users): " . count($users) . "</pre>\n";
+    // count($users) = 1
     
     //  Iterate through the array of user objects
     foreach( $user as $users )
     {
         // Try to display something
         echo "<p>" . $user->getId() . "</p>\n";
+        
+        echo "<p>I'm feeling loopy.</p>\n";
+        // Nothing's happening in the for loop.
     }
-    // Troubleshooting statement
-    echo "<pre>foreach executed.</pre>\n";
+    
+    var_dump($users);
+    /*
+    This got dumped:
+    array(1)
+    { [0]=> object(Identifier)#7 (4)
+        { ["id":"Child":private]=> string(8) "persicol" ["path":"Child":private]=> object(Path)#13 (3)
+            { ["path":"Path":private]=> string(0) "" ["site_id":"Path":private]=> NULL ["site_name":"Path":private]=> NULL
+            } ["type":"Child":private]=> string(4) "user" ["recycled":"Child":private]=> bool(false)
+        }
+    }
+    */
+    
 }
 catch(Exception $e) {
     echo S_PRE . $e . E_PRE;
